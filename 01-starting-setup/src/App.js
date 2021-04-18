e6c8d2eb66909b35d59cb66aeba28fb8725924c5
smarne <neelmarne2012@gmail.com>
2021-04-18 18:39:01 +0530

    split components

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,10 +1,27 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const arr = [
+    {date : new Date(), 
+    insurance : 'first' ,
+    amount : '$21.05'
+   },
+   {date : new Date(), 
+    insurance : 'second' ,
+    amount : '$356.05'
+   },
+   {date : new Date(), 
+    insurance : 'third' ,
+    amount : '$2125.05'
+   }
+  ];
+
   return (
     <div>
       <h2>Let's get started with react js !</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem date={arr[0].date} insurance={arr[0].insurance} amount={arr[0].amount}></ExpenseItem>
+      <ExpenseItem date={arr[1].date} insurance={arr[1].insurance} amount={arr[1].amount}></ExpenseItem>
+      <ExpenseItem date={arr[2].date} insurance={arr[2].insurance} amount={arr[2].amount}></ExpenseItem>
     </div>
   );
 }
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem()
+function ExpenseItem(props)
 {
-    const date = new Date();
-    const insurcane = 'car insurcane';
-    const amount = '$252.25';
+    
     return (
         <div className="expense-item">
-            <div className="expense-item__description">{date.toString()}</div>
-            <div className="expense-item__price">{insurcane}</div>
-            <div>{amount}</div>
+            <div className="expense-item__description   ">{props.date.toString()}</div>
+            <div className="expense-item__price">{props.insurance}</div>
+            <div>{props.amount}</div>
         </div>
     )
 
