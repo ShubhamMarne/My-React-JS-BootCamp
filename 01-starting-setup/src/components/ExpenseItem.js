a9eb48f1dab6a54bc292ca4ce611eae8404ed11f
smarne <neelmarne2012@gmail.com>
2021-04-19 02:14:48 +0530

    ways to update state

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -6,17 +6,32 @@ import Card from './Card'
 
 const ExpenseItem = (props) => 
 {
-    const [insurance, setTitle] = useState(props.insurance);
+    const [userInput, setUserInput] = useState({
+        insurance : props.insurance,
+        amount : props.amount
+    });
 
-    const clickHandler = () => {
-        setTitle('new insurance');
-        console.log(insurance);
+    const clickHandler = (event) => {
+        // only one state and state update will done by below function which will execute component function again
+        // 1. This is not recommended approach as if multiple times state is getting updated this will not show latest state
+        setUserInput({
+            ...userInput,
+            insurance : 'updated'
+        });
+        // 2. This is recommended approach as this will show latest state though multiple state update at a time.
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                insurance : 'updated'
+            }
+        });
+        
     }
 
     return (
         <Card className="expense-item">
             <div className="expense-item__description   ">{props.date.toString()}</div>
-            <div className="expense-item__price">{insurance}</div>
+            <div className="expense-item__price">{props.insurance}</div>
             <div>{props.amount}</div>
             <button onClick={clickHandler}>Click</button>
         </Card>
