687f696fdad25676c717b6e371d5566b8b12d7ac
smarne <neelmarne2012@gmail.com>
2021-04-28 18:56:59 +0530

    useReducer implmentation

---
+++ 04-project/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  console.log("application started");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+    console.log("login handler");
+  };
+
+
+  // USe effect first demo
+  // useEffect(() => {
+  //   console.log("in use effect");
+  // }, []);
+
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  console.log("return");
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
